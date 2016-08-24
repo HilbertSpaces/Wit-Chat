edb90a85ce583405ebb59b6b43c2de931708edfd
HilbertSpaces <Jktecon@gmail.com>
2016-08-24 12:53:19 -0400

    fix

--- server.js
+++ server.js
@@ -5,15 +5,21 @@ app.use(express.static('public'));
 var socket_io= require('socket.io');
 var http= require('http');
 app.use(express.static('public'));
+const {Wit,log}= require('node-wit');
+const client= new Wit({accessToken:'3LWGZX64CB5HO7RL2H4H4UCIFHPQ7W47'});
+'use strict';
 
 var server= http.Server(app);
 var io= socket_io(server);
 io.on('connection', function(socket){
 	console.log('Client connected');
 	socket.on('message', function(message){
-	    console.log('Received message:',message);
-	    socket.broadcast.emit ('message', message);
+		console.log(message);
+		socket.broadcast.emit('message',message);
+		client.converse('my-user-session-42', message,{}).then((data)=>{
+			io.emit('message',data.msg);
+		});
 	});
 });
-
 server.listen(8080);
+console.log("listening on 8080...");
